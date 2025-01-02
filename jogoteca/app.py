b644ba53b3d9ee358b86fe0e5d90f4ca0bdf8637
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-01-01 21:04:23 -0300

    feat: Add novo.html

--- jogoteca/app.py
+++ jogoteca/app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Super Mario', 'Ação', 'SNES')
+jogo2 = Jogo('Pokemon Gold', 'RPG', 'GBA')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'SNES')
+lista = [jogo1, jogo2, jogo3]
+
+@app.route('/lista')
 def home():
-    return render_template('lista.html')
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    list.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 if __name__ == "__main__":
     app.run(debug=True)
