e644658b894ae266eb200ecee8b9cbcc9a58b061
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-02-12 21:32:35 -0300

    feat: Add configurations

---
+++ auth-jwt/config.py
@@ -0,0 +1,16 @@
+import os
+from datetime import timedelta
+
+class Config:
+    # Usando o os para garantir que a pasta 'instance' exista e o caminho seja construído corretamente
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # Diretorio base
+    INSTANCE_FOLDER = os.path.join(BASE_DIR, 'instance')
+    
+    # Certifique-se de que a pasta 'instance' exista
+    if not os.path.exists(INSTANCE_FOLDER):
+        os.makedirs(INSTANCE_FOLDER)
+    
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(INSTANCE_FOLDER, "users.db")}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'supersecretkey'
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
