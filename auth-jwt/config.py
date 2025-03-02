f6bea378c6cb210c34a3a42c182f5c6607b29323
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-03-02 15:07:33 -0300

    refactor: Refactor config.py

--- auth-jwt/config.py
+++ auth-jwt/config.py
@@ -1,5 +1,9 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
+
+# Carregar as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 class Config:
     # Usando o "os" para garantir que a pasta 'instance' exista e o caminho seja construído corretamente
@@ -12,7 +16,10 @@ class Config:
     
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(INSTANCE_FOLDER, "users.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'supersecretkey'
+
+    # Acesso a chave secreta de forma segura
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fallback_secret_key')  # Valor de fallback se não encontrar no .env
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+    
     DEBUG = True
     FLASK_ENV = "development"  # Modo de ambiente do Flask
