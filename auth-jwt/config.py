9ab6ff0e684a04c6d5b6b9e677c849ca713b30b1
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-03-08 20:08:06 -0300

    refactor: Refactor config.py

--- auth-jwt/config.py
+++ auth-jwt/config.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 from dotenv import load_dotenv
 
-# Carregar as variáveis de ambiente do arquivo .env
+# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
 class Config:
@@ -16,6 +16,14 @@ class Config:
     
     SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(INSTANCE_FOLDER, "users.db")}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # Configuração do Pool de Conexão
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_size": 5,              # Número máximo de conexões no pool
+        "max_overflow": 10,          # Número máximo de conexões extras acima do pool_size
+        "pool_timeout": 30,          # Tempo máximo de espera por uma conexão livre (segundos)
+        "pool_recycle": 1800         # Tempo para reciclar conexões (segundos)
+    }
 
     # Acesso a chave secreta de forma segura
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'fallback_secret_key')  # Valor de fallback se não encontrar no .env
